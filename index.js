14ba26d590eb1cf3ce65a1f506ad570d3274cf79
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2023-10-18 17:30:31 -0600

    Hicimos modulo para menú

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
-const { makeGraph } = require('./modules/createGraph');
-const { dfsMethod } = require('./algorithms/dfs');
-const { bfsMethod } = require('./algorithms/bfs')
-const readline = require('readline');
-
-function menu(){
-    const options = [1,2,3,4];
-    const algorithms = ['DFS','BFS','A*','Minimax'];
-    let welcomeMessage = 'Seleccione el algoritmo:  \n';
-    for(let i = 0; i<algorithms.length; i++){
-        let algorithm = `${i+1}. ${algorithms[i]} \n`;
-        welcomeMessage = welcomeMessage + algorithm;
-    } 
-    console.log(welcomeMessage);
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question('Ingrese el número del método: ', (selectedMethod) => {
-        const method = parseInt(selectedMethod);
-        if (options.includes(method)) {
-            const graph = defGraph();
-            if(method == 1){
-                dfsMethod(graph,'A','C');
-            }else if(method == 2){
-                bfsMethod(graph, 'A', 'D');
-            }
-        } else {
-            console.log('Método no válido. Por favor, seleccione un método válido.');
-        }
-
-        rl.close();
-    });
-}
+const { menu } = require('./modules/menu');
 
 function main(){
     menu();
-}
-
-function defGraph(){
-    const nodes = ['A', 'B', 'C', 'D','E','F'];
-    const edges = [
-        { source: 'A', target: 'B',  },
-        { source: 'A', target: 'C',  },
-        { source: 'B', target: 'D',  },
-        { source: 'B', target: 'E',  },
-        { source: 'C', target: 'F',  },
-        { source: 'C', target: 'G',  },
-    ];
-    return makeGraph(nodes, edges);
 }
 
 main();
---
+++ modules/menu.js
@@ -0,0 +1,61 @@
+const readline = require('readline');
+const { makeGraph} = require('./createGraph')
+const { dfsMethod } = require('../algorithms/dfs');
+const { bfsMethod } = require('../algorithms/bfs');
+
+function menu(){
+
+    const options = message();
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Ingrese el número del método: ', (selectedMethod) => {
+        const method = parseInt(selectedMethod);
+        if (options.includes(method)) {
+            const graph = defGraph();
+            if(method == 1){
+                dfsMethod(graph,'A','C');
+            }else if(method == 2){
+                bfsMethod(graph, 'A', 'D');
+            }
+        } else {
+            console.log('Método no válido. Por favor, seleccione un método válido.');
+        }
+
+        rl.close();
+    });
+}
+
+function message(){
+    const options = [1,2,3];
+    const algorithms = ['DFS','BFS','Minimax'];
+    let welcomeMessage = 'Seleccione el algoritmo:  \n';
+    for(let i = 0; i<algorithms.length; i++){
+        let algorithm = `${i+1}. ${algorithms[i]} \n`;
+        welcomeMessage = welcomeMessage + algorithm;
+    } 
+    console.log(welcomeMessage);
+    return options;
+
+}
+
+function defGraph(){
+    const nodes = ['A', 'B', 'C', 'D','E','F'];
+    const edges = [
+        { source: 'A', target: 'B',  },
+        { source: 'A', target: 'C',  },
+        { source: 'B', target: 'D',  },
+        { source: 'B', target: 'E',  },
+        { source: 'C', target: 'F',  },
+        { source: 'C', target: 'G',  },
+    ];
+    return makeGraph(nodes, edges);
+}
+
+
+module.exports = {
+    menu
+}
