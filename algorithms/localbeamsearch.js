467ff3139bcd65bfb7de73eacc23c030842a8f26
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2023-12-12 14:36:36 -0600

    Agregamos nuevos algoritmos

---
+++ algorithms/localbeamsearch.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(state, heuristic) {
+        this.state = state;
+        this.heuristic = heuristic;
+    }
+}
+
+function localBeamSearch(graph, start, goal, beamWidth, maxIterations) {
+    let currentStates = [new Node(start, heuristic(graph, start, goal))];
+    
+    for (let iteration = 0; iteration < maxIterations; iteration++) {
+        let nextStates = [];
+        
+        for (let state of currentStates) {
+            if (state.state === goal) {
+                console.log(`¡Objetivo alcanzado! Estado actual: ${state.state}`);
+                return;
+            }
+
+            let successors = graph[state.state] || {};
+            let possibleStates = Object.keys(successors);
+
+            for (let nextState of possibleStates) {
+                let heuristicValue = heuristic(graph, nextState, goal);
+                let nextStateNode = new Node(nextState, heuristicValue);
+                nextStates.push(nextStateNode);
+            }
+        }
+
+        nextStates.sort((a, b) => a.heuristic - b.heuristic);
+        currentStates = nextStates.slice(0, beamWidth);
+        console.log(`Iteración ${iteration + 1}: ${currentStates.map(node => node.state)}`);
+    }
+
+    console.log("Alcanzado el número máximo de iteraciones sin llegar al objetivo.");
+}
+
+function heuristic(graph, current, goal) {
+    if (!graph[current] || !graph[goal]) {
+        return Infinity;
+    }
+    return graph[current][goal] || Infinity;
+}
+
+// Grafo proporcionado
+let graph = {
+    'A': { 'B': 3, 'C': 5 },
+    'B': { 'A': 2, 'D': 4, 'E': 7, 'F': 2 },
+    'C': { 'A': 6, 'F': 3 },
+    'D': { 'B': 8 },
+    'E': { 'B': 5, 'F': 6 },
+    'F': { 'C': 4, 'E': 1, 'G': 4 },
+    'G': {}
+};
+
+// Parámetros
+let startState = 'A';
+let goalState = 'G';
+let beamWidth = 3;
+let maxIterations = 5;
+
+// Ejecutar Búsqueda Local en Haz
+localBeamSearch(graph, startState, goalState, beamWidth, maxIterations);
---
+++ algorithms/ucs.js
@@ -0,0 +1,72 @@
+class Node {
+    constructor(state, cost, parent = null) {
+        this.state = state;
+        this.cost = cost;
+        this.parent = parent;
+    }
+}
+
+function ucs(graph, start, goal) {
+    let visited = new Set();
+    let priorityQueue = [];
+
+    let startNode = new Node(start, 0);
+    priorityQueue.push(startNode);
+
+    while (priorityQueue.length > 0) {
+        let currentNode = priorityQueue.shift();
+
+        if (currentNode.state === goal) {
+            let finalPath = [];
+            let cost = currentNode.cost;
+            while (currentNode) {
+                finalPath.unshift(currentNode.state);
+                currentNode = currentNode.parent;
+            }
+            return { path: finalPath, cost: cost };
+        }
+
+        if (!visited.has(currentNode.state)) {
+            visited.add(currentNode.state);
+
+            for (let neighbor in graph[currentNode.state]) {
+                let cost = graph[currentNode.state][neighbor];
+                let newCost = currentNode.cost + cost;
+                let neighborNode = new Node(neighbor, newCost, currentNode);
+
+                priorityQueue.push(neighborNode);
+                priorityQueue.sort((a, b) => a.cost - b.cost);
+            }
+        }
+    }
+
+    return { path: null, cost: null };
+}
+
+function findShortestPath(weightedGraph, startNode, goalNode) {
+    let result = ucs(weightedGraph, startNode, goalNode);
+
+    if (result.path) {
+        console.log(`\nCamino encontrado de ${startNode} a ${goalNode}: ${result.path}`);
+        console.log(`Costo total: ${result.cost}`);
+    } else {
+        console.log(`\nNo hay camino de ${startNode} a ${goalNode}`);
+    }
+}
+
+// Ejemplo de grafo ponderado en JavaScript
+let weightedGraph = {
+    'A': { 'B': 4, 'C': 2 },
+    'B': { 'A': 5, 'D': 7, 'E': 8, 'F': 1 },
+    'C': { 'A': 2, 'F': 4 },
+    'D': { 'B': 7 },
+    'E': { 'B': 8, 'F': 6 },
+    'F': { 'C': 4, 'E': 6 }
+};
+
+// Nodo de inicio y nodo objetivo
+let startNode = 'A';
+let goalNode = 'F';
+
+// Llamada a la función findShortestPath
+findShortestPath(weightedGraph, startNode, goalNode);
